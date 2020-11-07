29e1727d7d8e0b388f4eceeff838ec9e9f270d63
Taichi Kushiro <taichi.kushiro@nextbeat.net>
2020-11-07 21:22:51 +0900

    fixed SemigroupSyntax structure

--- src/main/scala/tabby/syntax/semigroup.scala
+++ src/main/scala/tabby/syntax/semigroup.scala
@@ -2,11 +2,9 @@ package tabby
 package syntax
 
 trait SemigroupSyntax {
-  implicit final def tabbySyntaxSemigroup[A: Semigroup](a: A): SemigroupOps[A] =
-    new SemigroupOps[A](a)
+  implicit class SemigroupOps[A](lhs: A) {
+    def |+|(rhs: A)(implicit sa: Semigroup[A]): A = combine(rhs)(sa)
+    def combine(rhs: A)(implicit sa: Semigroup[A]): A = sa.combine(lhs, rhs)
+  }
 }
 
-final class SemigroupOps[A](lhs: A) {
-  def |+|(rhs: A)(implicit sa: Semigroup[A]): A = combine(rhs)(sa)
-  def combine(rhs: A)(implicit sa: Semigroup[A]): A = sa.combine(lhs, rhs)
-}
