5c6208ed0bfeae3eaba2b2d6e0284f70d430d079
Taichi Kushiro <taichi.kushiro@nextbeat.net>
2020-11-07 18:53:35 +0900

    fixed small test

--- src/test/scala/tabby/instances/IntInstancesTest.scala
+++ src/test/scala/tabby/instances/IntInstancesTest.scala
@@ -3,9 +3,9 @@ package tabby.instances
 import org.scalatest.funsuite.AnyFunSuite
 import tabby.implicits._
 
-object IntInstancesTest extends AnyFunSuite {
+class IntInstancesTest extends AnyFunSuite {
   test("semigroup") {
     assert((1 combine 2) == 3)
-    assert((3 |+| 4) == 6)
+    assert((3 |+| 4) == 7)
   }
 }
