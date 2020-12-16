1e6b240e75c63f398d3775d677d37e8ed8238746
Taichi Kushiro <k.taichi0315@gmail.com>
2020-12-16 22:42:33 +0900

    add free test

--- src/main/scala/tabby/instances/EitherInstances.scala
+++
@@ -1,8 +0,0 @@
-package tabby
-package instances
-
-trait EitherInstances {
-  implicit val tabbyStdFunctorForEither: Functor[Either] = new Functor[Either] {
-    def map[A, B](fa: Either[A])(f: A => B): Option[B] = fa.map(f)
-  }
-}
--- src/test/scala/tabby/free/FreeTest.scala
+++ src/test/scala/tabby/free/FreeTest.scala
@@ -7,19 +7,42 @@ import tabby.implicits._
 
 class FreeTest extends AnyFunSuite {
 
-  test("simple map") {
-    val pure1: Free[Option, Int] = Pure(1)
+  test("case 1") {
+    val pure: Free[Option, Int] = Pure(1)
 
-    val f = pure1.map(p1 => p1 + 2)
+    val f = pure.flatMap(p => Pure(p + 2))
 
-    println(f)
+    assert(f == Pure(3))
   }
 
-  test("simple flatMap") {
-    val pure1: Free[Option, Int] = Pure(1)
+  test("case 2") {
+    val pure: Free[Option, Int] = Pure(1)
 
-    val f = pure1.flatMap(p1 => Pure(p1 + 2))
+    val f = pure.map(p => p + 2)
 
-    println(f)
+    assert(f == Pure(3))
   }
+
+  test("case 3") {
+    val pure1: Free[Option, Int] = Pure(1)
+    val pure2: Free[Option, Int] = Pure(2)
+
+    val f = pure1.flatMap(p1 =>
+      pure2.map(p2 => p1 + p2)
+    )
+
+    assert(f == Pure(3))
+  }
+
+  test("case 4") {
+    val pure1: Free[Option, Int] = Pure(1)
+    val pure2: Free[Option, Int] = Pure(2)
+
+    val f = for {
+      p1 <- pure1
+      p2 <- pure2
+    } yield p1 + p2
+
+    assert(f == Pure(3))
+  } 
 }
