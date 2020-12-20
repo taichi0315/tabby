ab59f3092d2b05a433f5c36dcd28cf02d212aa31
Taichi Kushiro <k.taichi0315@gmail.com>
2020-12-20 14:44:33 +0900

    fxed type parameter name F to S on Free

--- src/main/scala/tabby/free/Free.scala
+++ src/main/scala/tabby/free/Free.scala
@@ -1,16 +1,16 @@
 package tabby
 package free
 
-sealed trait Free[F[_], A] {
+sealed trait Free[S[_], A] {
 
-  def map[B](f: A => B)(implicit F: Functor[F]): Free[F, B] = flatMap(a => Pure(f(a)))
+  def map[B](f: A => B)(implicit S: Functor[S]): Free[S, B] = flatMap(a => Pure(f(a)))
 
-  def flatMap[B](f: A => Free[F, B])(implicit F: Functor[F]): Free[F, B] =
+  def flatMap[B](f: A => Free[S, B])(implicit F: Functor[S]): Free[S, B] =
     this match {
       case Pure(a) => f(a)
-      case Impure(ff) => Impure(F.map(ff)(_.flatMap(f)))
+      case FlatMapped(ff) => FlatMapped(F.map(ff)(_.flatMap(f)))
     }
 }
 
-case class Pure[F[_], A](a: A) extends Free[F, A]
-case class Impure[F[_], A](ff: F[Free[F, A]]) extends Free[F, A]
+case class Pure[S[_], A](a: A) extends Free[S, A]
+case class FlatMapped[S[_], A](ff: S[Free[S, A]]) extends Free[S, A]
