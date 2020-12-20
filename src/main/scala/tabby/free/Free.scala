b59c9cc70f537be71fafe8729dc05bd4e9c5c801
Taichi Kushiro <k.taichi0315@gmail.com>
2020-12-20 15:33:57 +0900

    fixed conflict

--- src/main/scala/tabby/free/Free.scala
+++ src/main/scala/tabby/free/Free.scala
@@ -1,21 +1,16 @@
 package tabby
 package free
 
-sealed trait Free[S[_], A] {
-  import Free.{Pure, FlatMapped}
+sealed trait Free[F[_], A] {
 
-  def map[B](f: A => B)(implicit S: Functor[S]): Free[S, B] = flatMap(a => Pure(f(a)))
+  def map[B](f: A => B)(implicit F: Functor[F]): Free[F, B] = flatMap(a => Pure(f(a)))
 
-  def flatMap[B](f: A => Free[S, B])(implicit F: Functor[S]): Free[S, B] =
+  def flatMap[B](f: A => Free[F, B])(implicit F: Functor[F]): Free[F, B] =
     this match {
       case Pure(a) => f(a)
-      case FlatMapped(ff) => FlatMapped(F.map(ff)(_.flatMap(f)))
+      case Impure(ff) => Impure(F.map(ff)(_.flatMap(f)))
     }
 }
 
-object Free {
-  case class Pure[S[_], A](a: A) extends Free[S, A]
-  case class FlatMapped[S[_], A](ff: S[Free[S, A]]) extends Free[S, A]
-}
-
-
+case class Pure[F[_], A](a: A) extends Free[F, A]
+case class Impure[F[_], A](ff: F[Free[F, A]]) extends Free[F, A]
